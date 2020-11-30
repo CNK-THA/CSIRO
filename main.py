6d15bfc7adf5fb9570e8893c05f027e66e628f1a
CNK-THA <47205250+CNK-THA@users.noreply.github.com>
2020-11-30 14:37:41 +1000

    Json creator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
+# import pandas as pd
 import json
 
 
@@ -26,13 +26,39 @@ from pathlib import Path
 # print(df.iloc[100])
 
 
+
+# class Jsonable(object):
+#     def __iter__(self):
+#         for attr, value in self.__dict__.iteritems():
+#             if isinstance(value, datetime.datetime):
+#                 iso = value.isoformat()
+#                 yield attr, iso
+#             elif isinstance(value, decimal.Decimal):
+#                 yield attr, str(value)
+#             elif(hasattr(value, '__iter__')):
+#                 if(hasattr(value, 'pop')):
+#                     a = []
+#                     for subval in value:
+#                         if(hasattr(subval, '__iter__')):
+#                             a.append(dict(subval))
+#                         else:
+#                             a.append(subval)
+#                     yield attr, a
+#                 else:
+#                     yield attr, dict(value)
+#             else:
+#                 yield attr, value
+
+
 class Country:
     def __init__(self, countryCode):
         self.countryCode = countryCode
-        self.regions = set()
+        self.regions = {}
         
     def add_region(self, region):
-        self.regions.add(region)
+        key = region.regionCode
+        if self.regions.get(key) == None:
+            self.regions[key] = region
         
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
@@ -41,28 +67,28 @@ class Country:
 class Region:
     def __init__(self, regionFIPS):
         self.regionCode = regionFIPS
-        self.subRegions = set()
+        self.subRegions = {}
 
     def add_sub_region(self, region):
-        self.subRegions.add(region)
+        key = region.regionCode
+        if self.subRegions.get(key) == None:
+            self.subRegions[key] = region
     
     def __eq__(self, other):
         return self.regionCode == other.regionCode
 
     def __hash__(self):
         return hash(self.regionCode)
-  
 
-class Region2:
-    def __init__(self, region):
-        self.region = region
-        
+class SmallestRegion:
+    def __init__(self, regionFIPS):
+        self.regionCode = regionFIPS
+
     def __eq__(self, other):
-        return self.region == other.region
+        return self.regionCode == other.regionCode
 
     def __hash__(self):
-        return hash(self.region)
-
+        return hash(self.regionCode)
 
 
 
@@ -70,10 +96,10 @@ current = None
 
 
 
-with open(str(Path.home())+"/Downloads/allCountries.txt", 'r', encoding="utf8", errors='ignore') as dataFile:
+with open("allCountries.txt", 'r', encoding="utf8", errors='ignore') as dataFile: #str(Path.home())+"/Downloads/
     for line in dataFile:
         d = line.split("\t")
-        print(d)
+        # input(d)
         
             
         if len(d) != 19: # Expecting 19 columns
@@ -86,20 +112,69 @@ with open(str(Path.home())+"/Downloads/allCountries.txt", 'r', encoding="utf8", 
 
 ##        print(d[11]) #DIVISION 2??
 
+
         
         #if this is the first one or have moved on to next country
         if current == None or current.countryCode != d[8]:
             if current != None:
 ##                input(current.regions)
-                current.regions = list(current.regions) # make it serialisable
+                # current.regions = list(current.regions) # make it serialisable
                 output = open("test.txt", "a")
                 output.write(current.toJSON())
                 output.close()
-            current = Country(d[8])
-            
-        else:
-            current.add_region(Region(d[10]))
-    
+
+            current = Country(d[8]) #parse country
+
+            currentRegion = Region(d[10])  # parse region level 1
+            current.add_region(currentRegion)
+
+            currentRegion2 = Region(d[11])  # parse region level 2
+            currentRegion.add_sub_region(currentRegion2)
+
+            currentRegion3 = Region(d[12])  # parse region level 3
+            currentRegion2.add_sub_region(currentRegion3)
+
+            currentRegion4 = SmallestRegion(d[13])  # parse region level 4
+            currentRegion3.add_sub_region(currentRegion4)
+
+            # print(current.regions, currentRegion.subRegions, currentRegion2.subRegions, currentRegion3.subRegions, currentRegion4.subRegions)
+
+        else: #same country still
+
+            currentRegion = Region(d[10])  # parse region level 1
+            currentRegion2 = Region(d[11])  # parse region level 2
+            currentRegion3 = Region(d[12])  # parse region level 3
+            currentRegion4 = SmallestRegion(d[13])  # parse region level 4
+
+            # print(current.regions.keys())
+            g = current.regions.get(d[10])
+
+            # print(g)
+            if g is None: #doesn't exist, add them
+                current.add_region(currentRegion)
+            else: #already exist, use the new one
+                currentRegion = g
+
+            g2 = currentRegion.subRegions.get(d[11])
+            if g2 is None:  # doesn't exist, add them
+                currentRegion.add_sub_region(currentRegion2)
+            else:  # already exist, use the new one
+                currentRegion2 = g2
+
+            g3 = currentRegion2.subRegions.get(d[12])
+            if g3 is None:  # doesn't exist, add them
+                currentRegion2.add_sub_region(currentRegion3)
+            else:  # already exist, use the new one
+                currentRegion3 = g3
+
+            g4 = currentRegion3.subRegions.get(d[13])
+            if g4 is None:  # doesn't exist, add them
+                currentRegion3.add_sub_region(currentRegion4)
+            else:  # already exist, use the new one
+                currentRegion4 = g4
+
+
+
 
 
 
