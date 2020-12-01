c7352157f3f46e523d28e25514221bea43894174
CNK-THA <47205250+CNK-THA@users.noreply.github.com>
2020-12-01 13:21:40 +1000

    Up to Suburbs

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ from pathlib import Path
 #                 yield attr, value
 
 
-class Country:
+class Location:
     def __init__(self, countryCode):
         self.countryCode = countryCode
         self.regions = {}
@@ -64,36 +64,35 @@ class Country:
         return json.dumps(self, default=lambda o: o.__dict__, 
             sort_keys=True, indent=4)
 
-class Region:
-    def __init__(self, regionFIPS):
-        self.regionCode = regionFIPS
+class SmallestRegion:
+    def __init__(self, regionName, regionCode, parent):
+        self.regionName = regionName
+        self.regionCode = regionCode
+        self.parentCode = parent
         self.subRegions = {}
 
     def add_sub_region(self, region):
-        key = region.regionCode
-        if self.subRegions.get(key) == None:
+        key = region.regionName
+        if self.subRegions.get(key) is None:
             self.subRegions[key] = region
-    
-    def __eq__(self, other):
-        return self.regionCode == other.regionCode
-
-    def __hash__(self):
-        return hash(self.regionCode)
-
-class SmallestRegion:
-    def __init__(self, regionFIPS):
-        self.regionCode = regionFIPS
 
     def __eq__(self, other):
-        return self.regionCode == other.regionCode
+        return self.regionName == other.regionName
 
     def __hash__(self):
-        return hash(self.regionCode)
+        return hash(self.regionName)
+
+    def __str__(self):
+        return self.regionName + ", " + self.regionCode + ", "
 
 
 
 current = None
 countriesList = {}
+
+
+temporaryList = []
+temporaryList3 = [] #level 3
 with open("countryInfo.txt", 'r', encoding="utf8") as countryFile:
     lineNumber = 0
     for line1 in countryFile:
@@ -112,24 +111,78 @@ with open("allCountries.txt", 'r', encoding="utf8", errors='ignore') as dataFile
         d = line.split("\t")
 
 
+
         
             
         if len(d) != 19: # Expecting 19 columns
             print(d)
             input("STOP")
-        if d[6] != 'A': #ONLY GET CLASS A INFORMATION
+        if d[6] != 'P' and d[6] != 'A': #ONLY GET CLASS A INFORMATION
             continue
+
+        # try:
+        #     d.index("AU")
+        #     d.index("PPLX")
+        #     # d.index("ADM3")
+        #     # print(d[7])
+        #     # print(d[7] == "ADM")
+        #     # # d.index("US")
+        #     print(d)
+        # except:
+        #     pass
+
 
         # input(d)
         
         #if this is the first one or have moved on to next country
-        if current == None or current.countryCode != countriesList.get(d[8]):
-            if current != None:
-                output = open("test.txt", "a")
-                output.write(current.toJSON())
-                output.close()
+        if current == None:
+            current = Location(countriesList.get(d[8]))
+        elif current != None and current.countryCode != countriesList.get(d[8]):
 
-            current = Country(countriesList.get(d[8])) #parse country, CHECK NULL VALUE??
+
+            for stuff in temporaryList: #delete from list so better efficiency?? Same with nested
+
+                try: #SOME REGIONS CAN"T FIND THEIR PARENTS!! FIX!!
+                    top = current.regions.get(stuff.parentCode)
+                    if current.countryCode == "Australia" and top is None:
+                        print(stuff)
+                        print(current.regions)
+                        print(top)
+                        input('stop')
+
+
+
+                    top.add_sub_region(stuff)
+
+                    for stuff2 in temporaryList3:
+                        if stuff2.parentCode == stuff.regionCode:
+                            stuff.add_sub_region(stuff2)
+
+                    # if current.countryCode == "Australia":
+                    #     print(stuff)
+                    #     print(top)
+                    #     print(top.subRegions)
+                    #
+                    #     print("For each states we have")
+                    #     print(top.subRegions.get(3).subRegions)
+                    #     input('pause')
+
+
+                except:
+                    pass
+
+
+
+
+
+
+            temporaryList = []
+
+            output = open("test.txt", "a")
+            output.write(current.toJSON())
+            output.close()
+
+            current = Location(countriesList.get(d[8])) #parse country, CHECK NULL VALUE??
 
             # if d[10] != '':
             #     currentRegion = Region(d[10])  # parse region level 1
@@ -148,11 +201,24 @@ with open("allCountries.txt", 'r', encoding="utf8", errors='ignore') as dataFile
             #     currentRegion3.add_sub_region(currentRegion4)
 
 
-        else: #same country still
+        # else: #same country still
             # print(d[2])
             # print(current)
-            currentRegion = SmallestRegion(d[2]) #level 1
+        if d[7] == "ADM1":
+            currentRegion = SmallestRegion(d[2], d[10], d[8]) #level 2, STATES
             current.add_region(currentRegion)
+        elif d[7] == "ADM2":
+            currentRegion = SmallestRegion(d[2], d[11], d[10])
+            temporaryList.append(currentRegion)
+        elif d[7] == "PPLX":
+            currentRegion = SmallestRegion(d[2], "XXX", d[11]) #DOUBLE CHECK THIS
+            temporaryList3.append(currentRegion)
+            # top = current.regions.get(d[10])
+            # currentRegion = SmallestRegion(d[2], d[10])
+            # if top is None:
+            #     temporaryList.append(currentRegion)
+            # else:
+            #     top.add_sub_region(currentRegion)
 
 
 
